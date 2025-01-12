7e4caa630467fc2ef8dd7d0b8ea59aece4717a5b
Amin <akbariamin01@gmail.com>
2025-01-12 13:08:32 +0330

    take User From DataBase

--- OnlineShop.Web/Areas/Admin/Controllers/HomeController.cs
+++ OnlineShop.Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OnlineShop.Data.Context;
 using OnlineShop.Domin.Entities.Users;
 using OnlineShop.Model.ViewModel.User;
 using OnlineShop.Service.Users;
@@ -8,28 +7,27 @@ namespace OnlineShop.Web.Areas.Admin.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ShopContext _context;
         private readonly IUserService _userService;
-        public HomeController(ShopContext context,IUserService userService)
+        public HomeController(IUserService userService)
         {
-            _context = context;
             _userService = userService;
         }
         [Area("Admin")]
         public async Task<ActionResult<User>> Index()
         {
-            var users = await _userService.GetUsersAsync().Select(s => new EditViewModel
+            var users = await _userService.GetUsersAsync();
+            var result= users.Select(s => new EditViewModel
             {
-                Id=s.Id,
-                FullName=s.FullName,
-                Email=s.Email,
-                Address=s.Address,
-                Order=s.order,
-                IsDeleted=false,
-                Password=s.Password
+                Id = s.Id,
+                FullName = s.FullName,
+                Email = s.Email,
+                Address = s.Address,
+                Order = s.Order,
+                IsDeleted = false,
+                Password = s.Password
 
-            });
-            return View();
+            }).ToList();
+            return View("Index", result);
         }
 
     }
