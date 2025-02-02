29f67e9a923a0beb497e5138d75f28356569791b
Amin <akbariamin01@gmail.com>
2025-02-02 13:46:03 +0330

    Data Anotion

--- OnlineShop.Model/ViewModel/Category/CategoryViewModel.cs
+++ OnlineShop.Model/ViewModel/Category/CategoryViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace OnlineShop.Model.ViewModel.Category
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShop.Model.ViewModel.Category
 {
     public class CategoryViewModel
     {
         public long Id { get; set; }
+        [Display(Name = "اسم دسته")]
+        [Required(ErrorMessage = "{0} را وارد کنید")]
         public string Title { get; set; }
         public bool IsDeleted { get; set; }
     }
